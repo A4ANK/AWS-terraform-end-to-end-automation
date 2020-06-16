ce35f55828cf20eb4da739d88e53300eab3e47de
A4ANK <33590451+A4ANK@users.noreply.github.com>
2020-06-16 12:29:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <pre>
 <?php
-<img src="https://de12d54af33958fda0b48c48b5b2be42340593bc849b006d8418f0ba3d619839.s3.ap-south-1.amazonaws.com/my.jpg"
+echo '<img src="https://de12d54af33958fda0b48c48b5b2be42340593bc849b006d8418f0ba3d619839.s3.ap-south-1.amazonaws.com/my.jpg">';
 
 print `ifconfig`;
 
